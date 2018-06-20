61e2b4242b4bd2f1f0408a45600023b4a075785f
Timdpr <QOTimSA@live.co.uk>
2018-06-20 11:51:48 +0100

    Add images and image crop/resize/binarise code

---
+++ images/plot_threshold.py
@@ -0,0 +1,30 @@
+"""
+Uses Otsu thresholding to convert all greyscale images in the
+specified directory to binary images - ie. black-and-white - and saves them.
+
+@author tp275
+"""
+
+import os
+import matplotlib.pyplot as plt
+from skimage import filters
+from skimage import io
+from skimage import color
+
+path = ('C:/Users/s/Documents/Masters/Project/images/REPLACE AS NEEDED/')
+dirs = os.listdir( path )
+
+def threshold():
+    for item in dirs:  # for each image
+        if os.path.isfile(path+item):
+            im = io.imread(path+item)
+
+            # Convert to binary using skimage filter
+            val = filters.threshold_otsu(im)
+            im = im < val
+
+            # Save, overwriting existing file
+            f, e = os.path.splitext(path+item)
+            plt.imsave(f, color.rgb2gray(im), cmap='gray')
+
+threshold()
---
+++ images/resizer.py
@@ -0,0 +1,26 @@
+"""
+Crops, resizes and saves all images in the specified directory. Used to
+downsize simulated ant views.
+
+@author tp275
+"""
+
+import os
+from PIL import Image
+
+path = ('C:/Users/s/Documents/Masters/Project/images/REPLACE AS NEEDED/')
+dirs = os.listdir( path )
+
+def crop_resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+			
+            w, h = im.size
+            im = im.crop((0, 9, w, h-9))
+			
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((90,5), Image.ANTIALIAS)
+            imResize.save(f + '.png', 'PNG')
+
+crop_resize()
