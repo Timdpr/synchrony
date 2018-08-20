53ebc0fc3a2984c0087078d490decb4abd95ea16
Timdpr <QOTimSA@live.co.uk>
2018-08-20 10:35:18 +0000

    Add images and setup for (boxes) distance experiments

--- navigation/individual_views/runExperiment.py
+++ navigation/individual_views/runExperiment.py
@@ -16,8 +16,8 @@ def outputResultsTextFile(results, result_num):
     	f.write(str(results))
         
 
-test = TestPatternsOnNetwork([7, 90], 5, rotation=True, rot_step=2,
-                             pattern_dir='/home/ec2-user/environment/synchrony/images/exp_parameter_tuning/',
+test = TestPatternsOnNetwork([7, 90], 5, rotation=False, rot_step=2,
+                             pattern_dir='/home/ec2-user/environment/synchrony/images/exp_distance/boxes/0/',
                              route_pattern_dir='/home/ec2-user/environment/synchrony/images/routes/route_boxes_90x7/',
 #                             pattern_dir='/mnt/hgfs/Masters/Project/synchrony/images/exp_rotation_rsync/',
 #                             route_pattern_dir='/mnt/hgfs/Masters/Project/synchrony/images/routes/route_boxes_90x7/',
@@ -43,7 +43,7 @@ for i in range(2):  # for i = image number
 #degrees = [180 for i in range(493)]
 #degrees_test = []
 
-
+"""
 for i in range(2):  # for i = image number
     # Create list of results of each sample for the particular image
     results_by_image = [sample[i] for sample in results]
@@ -52,7 +52,7 @@ for i in range(2):  # for i = image number
 #    degrees.append(degree)
 #    degrees_test.append(degree)
 #    rsync_test.append(rsync)
-
+"""
 
     
 #rsyncs[6][7] = rsync_test[0]
@@ -66,4 +66,4 @@ missing_img_nums = [slice(37, 42), slice(46, 50), slice(52, 59), slice(61, 76), 
                     slice(417, 425), slice(434, 442), slice(451, 458), slice(469, 473)]
 #quiver_plot(degrees, rsyncs, missing_img_nums)
     
-outputResultsTextFile(results, 1)
+outputResultsTextFile(results, 0)
